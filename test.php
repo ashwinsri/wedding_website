c711b902631bf38001629bd7d710ffd1ba80888b
Manisha Narayanan <manisha@minminicorp.com>
2014-04-24 22:02:09 -0700

    php

--- test.php
+++ test.php
@@ -2,11 +2,10 @@
 ini_set('display_errors',1); 
  error_reporting(E_ALL);
 
-//$name = $_POST['name'];
-//$date = $_POST['time'];
-//$file = $_POST['filename'];
-//$filepath = 'content/'.$file;
-$xml = file_get_contents('content/left_pane.xml');
+
+$file = $_POST['filename'];
+$filepath = 'content/'.$file;
+$xml = file_get_contents($filepath);
 
 echo $xml;
 ?>
