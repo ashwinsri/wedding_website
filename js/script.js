0f56c35e4026c91a011c5baa41c24eb0f454d3a5
Ashwin. S. Sampathkumar <Sampathkumar_Ashwin_S@cat.com>
2014-05-05 16:21:36 -0500

    Intermediate commit #3 - sorry about this.

--- js/script.js
+++ js/script.js
@@ -122,16 +122,34 @@ $.fn.getContent = function(file, element)
 	
 	if($(this).attr('id') == 'rsvp')
 	{
-		
-		var fullname = $('rsvp').find('#fullname').css('background-color','#ffa');
-		//console.log("fullname value = "+fullname);
-		/*if(($('#fullname').val().length > 0) && ($('#email').val().length > 0))
-		{
-			$("#submitChennai_Disabled").prop("id","submitChennai");
-			$("#submitSeattle_Disabled").prop("id","submitSeattle");
-		} */
+		$(this).validateForm();
+		$(this).sendRSVP();
 	}
 }
+
+
+$.fn.validateForm = function() 
+{
+    /*$('#rsvp table > input').keyup(function() {
+
+        var empty = false;
+        $('form > input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#register').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#register').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    }); */
+	
+	console.log("Form validated!")
+	
+}
+
 
 /*******************************************
 Name: sendRSVP
