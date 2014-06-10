1f711a72f1b2b63f03c9484144978d09f63e068c
manisha <manisha@minminicorp.com>
2014-06-09 22:34:37 -0700

    info page

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ $.fn.enPageFns = function()
 	});
 	
 	$('#gb').click(function () {
-	    $(document).getContent('guestbook.xml', 'content');
+	    $(document).getContent('info.xml', 'content');
 		$('.content').attr('class','col-lg-9 col-md-9 col-sm-9 col-xs-9 content flash guestbook');
 	});
 }
